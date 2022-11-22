9ed36fba9b547cd5656b1a07b7d8b5c6454c4e9a
zacki744 <78082188+zacki744@users.noreply.github.com>
2022-11-22 16:00:21 +0100

    new

--- semNy.c
+++ semNy.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h> /* For O_* constants */
 
+const char *semName1 = "my_sema1";
 #define SHMSIZE 128
 #define SHM_R 0400
 #define SHM_W 0200
@@ -15,9 +20,9 @@ int main(int argc, char **argv)
 {
 	struct shm_struct {
 		int buffer[10];
-		unsigned empty;
+		unsigned int empty;
+        sem_t *ocopied;
 	};
-	srand(time(NULL));
 	volatile struct shm_struct *shmp = NULL;
 	char *addr = NULL;
 	pid_t pid = -1;
@@ -27,37 +32,29 @@ int main(int argc, char **argv)
 	/* allocate a chunk of shared memory */
 	shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
 	shmp = (struct shm_struct *) shmat(shmid, addr, 0);
+    sem_init(shmp->ocopied, 0, 1);
+
 	shmp->empty = 0;
 	pid = fork();
 	if (pid != 0) {
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
+            sem_wait(&shmp->ocopied);
 			var1++;
-			//if(shmp->empty == 10)
-				//while (shmp->empty > 0); /* busy wait until the buffer is empty */
 			printf("Sending %d\n", var1); fflush(stdout);
 			shmp->buffer[(shmp->empty ++) % SIZE_OF_BUFFER] = var1;
-			int r = rand() % (5 - 1 + 1) + 1;
-			sleep(r/10);
+            sem_post(&shmp->ocopied);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	} else {
 		/* here's the child, acting as consumer */
 		while (var2 < 100) {
-			/* read from shmem */
-			//if (shmp->empty == 0)
-				//while (shmp->empty < 10); /* busy wait until there is something */
-			//
+            sem_wait(shmp->ocopied);
 			var2 = shmp->buffer[(shmp->empty ++) % SIZE_OF_BUFFER];
 			printf("Received %d\n", var2); fflush(stdout);
-			//designates a random number
-			int r = rand() % (20 - 2 + 1) + 2;
-			//sleap for a time
-			sleep(r/10);
-			shmdt(addr);
-			shmctl(shmid, IPC_RMID, shm_buf);
+            sem_post(shmp->ocopied);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);	
